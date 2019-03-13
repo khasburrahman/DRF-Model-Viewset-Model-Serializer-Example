be3bb7d4f517b358a6cf15be46ac7dc4f6f7e959
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-03-13 23:48:54 +0700

    add view set for example model

--- DRF_ModelViewSet_Responsive_Example/model_example/views.py
+++ DRF_ModelViewSet_Responsive_Example/model_example/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from model_example.serializers import ExampleSerialzier
+from model_example.models import Example
 
-# Create your views here.
+class ExampleViewSet(viewsets.ModelViewSet):
+    serializer_class = ExampleSerialzier
+    queryset = Example.objects.all()
+
+
