ca1b36cca97e5b3a76a1efff6bd0c297854f8e4a
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-03-13 22:50:10 +0700

    add example model

--- DRF_ModelViewSet_Responsive_Example/model_example/models.py
+++ DRF_ModelViewSet_Responsive_Example/model_example/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Example(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=50)
+    fielda = models.CharField(max_length=50)
+    fieldb = models.CharField(max_length=50)
+    fieldc = models.CharField(max_length=50)
