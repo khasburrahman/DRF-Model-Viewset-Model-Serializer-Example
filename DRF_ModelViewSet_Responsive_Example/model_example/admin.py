42e2d89e15bfc3091e5171449c08e18550941bb0
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-03-13 22:50:38 +0700

    register example model in admin

--- DRF_ModelViewSet_Responsive_Example/model_example/admin.py
+++ DRF_ModelViewSet_Responsive_Example/model_example/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+for model in apps.get_app_config('model_example').models.values():
+	admin.site.register(model)
