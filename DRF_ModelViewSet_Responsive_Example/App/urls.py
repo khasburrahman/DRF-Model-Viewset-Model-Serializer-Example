3b5cd22f12c65b3b49b51c085ebd2cd4d4234b8a
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-03-13 23:49:32 +0700

    add the model_example route to App

--- DRF_ModelViewSet_Responsive_Example/App/urls.py
+++ DRF_ModelViewSet_Responsive_Example/App/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	url(r'^model_example_app/', include(('model_example.urls', 'model_example'), namespace='model_example')),
 ]
+
