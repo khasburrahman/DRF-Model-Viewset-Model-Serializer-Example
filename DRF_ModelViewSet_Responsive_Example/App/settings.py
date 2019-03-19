15d51b004c1c6b62b9f8751c8d919f445323ad6b
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-03-19 23:22:19 +0700

    django settings change database engine to postgres

--- DRF_ModelViewSet_Responsive_Example/App/settings.py
+++ DRF_ModelViewSet_Responsive_Example/App/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'App.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_DB', ''),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', '')
     }
 }
 
