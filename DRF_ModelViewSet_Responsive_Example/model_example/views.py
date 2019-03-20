250374b31de2976a0966c9bde4cdcb9985a8cb94
Kharis Khasburrahman <khasburrahman@yahoo.com>
2019-03-20 23:43:36 +0700

    override get_serializer_context example

--- DRF_ModelViewSet_Responsive_Example/model_example/views.py
+++ DRF_ModelViewSet_Responsive_Example/model_example/views.py
@@ -7,4 +7,15 @@ class BookViewSet(viewsets.ModelViewSet):
     serializer_class = BookSerialzier
     queryset = Book.objects.all()
 
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        action = self.action
+        if (action == 'list'):
+            context['fields'] = ('id', 'title', 'price',)
+        elif (action == 'create'):
+            context['fields'] = ('id',)
+        elif (action == 'retrieve'):
+            context['fields'] = ('title', 'author', 'isbn', 'price', 'synopsis')
+        return context
+
 
